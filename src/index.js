e2f05c199583317e7af161d4f128f9508c2e03ac
Dmitry Strezh <36566007+dstressed@users.noreply.github.com>
2018-02-27 20:45:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
 module.exports = function count(string, pairs) {
+    var mask = string.split('');
+    var n = getN(pairs);
+    var counter = 0;
+
+    outer: for (var i = 1; i <= n; i++) {
+        for (var b = 0; b < mask.length; b++) {
+            if (mask[b] == 0) {
+                if (getGCD(i + b , n) == 1) continue outer;
+            }
+            if (mask[b] == 1) {
+                if (getGCD(i + b , n) != 1) continue outer;
+            }
+        }
+        counter++;
+    }
+
+    for (i = 0; i < pairs.length; i++) {
+        var integer = pairs[i][0];
+        var power = pairs[i][1];
+        for (b = 0; b < power - 1; b++) {
+            counter = (counter * integer) % 1000000007;
+        }
+    }
+
+    function getN(pairs) {
+        var n = pairs[0][0];
+        for (var i = 1, length = pairs.length ; i < length; i++) {
+            n *= pairs[i][0];
+        }
+        return n;
+    }
+
+    function getGCD(a, b) {
+        if (!b) {
+            return a;
+        }
+        return getGCD(b, a % b);
+    };
+
+    return counter % 1000000007;
+};
+
+/*
+module.exports = function count(string, pairs) {
   var mask = string.split('');
   var n = getN(pairs);
   var counter = 0;
@@ -47,3 +91,4 @@ module.exports = function count(string, pairs) {
   }
   return counter % 1000000007;
 };
+*/
